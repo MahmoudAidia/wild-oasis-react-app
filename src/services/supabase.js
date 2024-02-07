4f73156d69eaeaeea7e0182d2f652f474638df2d
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-02-07 10:06:16 +0200

    Connected the project with supbase database

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://cosvnlrvutdbwpfcdwbe.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvc3ZubHJ2dXRkYndwZmNkd2JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyMzEyMDEsImV4cCI6MjAyMjgwNzIwMX0.1_Agsbaq1EDS-aWAPRxdPcnpzmpLUM4Blt7wzyaFoy0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
