8d58963edf440f1ac4bea0c51dcab3961e7241d4
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-02-12 10:54:12 +0200

    Installed react-hook-form and registered all the input fields

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,15 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
   // In our layout we always need the pages to be placed without a parent components in order to make the styles work, so that's why we use the Fragment.
+
+  const [showForm, setShowFrom] = useState(false);
   return (
     <Fragment>
       <Row type="horizontal">
@@ -14,6 +18,15 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button
+          onClick={() => {
+            setShowFrom((showForm) => !showForm);
+          }}
+        >
+          {showForm ? "Close Form" : "Add a new cabin"}
+        </Button>
+
+        {showForm && <CreateCabinForm />}
       </Row>
     </Fragment>
   );
