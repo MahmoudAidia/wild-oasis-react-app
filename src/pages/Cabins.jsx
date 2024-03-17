f3266fa08bdb6cd47328719315c7a9045cf57fd5
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-03-17 14:34:09 +0200

    Created a filter component to filter a cabins data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -4,6 +4,7 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   // In our layout we always need the pages to be placed without a parent components in order to make the styles work, so that's why we use the Fragment.
@@ -12,7 +13,7 @@ function Cabins() {
     <Fragment>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTable />
