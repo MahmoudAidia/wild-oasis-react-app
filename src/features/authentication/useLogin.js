812705ec07aace6e7e19902cc7dc7fab64f752fa
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-05-02 11:26:06 +0300

    Added functionality to login users to supabase

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,23 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+function useLogin() {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: () => {
+      navigate("/dashboard");
+    },
+
+    onError: (err) => {
+      toast.error("Provided email or password is incorrect");
+      console.log("Error", err);
+    },
+  });
+
+  return { login, isLoading };
+}
+
+export default useLogin;
