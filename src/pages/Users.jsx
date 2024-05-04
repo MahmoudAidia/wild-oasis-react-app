5c853b0734a881a0016097c090eb2d0985fe741d
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-05-04 12:40:57 +0300

    Implemented Signup functionality

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,14 @@
+import { Fragment } from "react";
 import Heading from "../ui/Heading";
+import SignupForm from "../features/authentication/SignupForm";
 
 function NewUsers() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <Fragment>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm />
+    </Fragment>
+  );
 }
 
 export default NewUsers;
