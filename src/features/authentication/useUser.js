4891cd42b73c6129d05c4f94597c948c4fada101
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-05-04 14:01:28 +0300

    Implemented application header

--- src/features/authentication/useUser.js
+++ src/features/authentication/useUser.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser } from "../../services/apiAuth";
 
-export function useUser() {
+function useUser() {
   const { isLoading, data: user } = useQuery({
     queryKey: ["user"],
     queryFn: getCurrentUser,
