ee09da13bce9f5b07202404a1867a3267f809051
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-05-08 12:11:40 +0300

    Implemented area chart to display the sales

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import useCabins from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,7 +34,7 @@ function DashboardLayout() {
       />
       <div>Today's Activity</div>
       <div>Chart Stay duration</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
