cc7dbf4ef43673c8984914fc3028d491eb8faa2f
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-05-13 10:24:49 +0300

    Adding animation to the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import LoginForm from "../features/authentication/LoginForm";
 import Logo from "../ui/Logo";
 import Heading from "../ui/Heading";
-
+import { motion } from "framer-motion";
 const LoginLayout = styled.main`
   min-height: 100vh;
   display: grid;
@@ -16,9 +16,27 @@ const LoginLayout = styled.main`
 function Login() {
   return (
     <LoginLayout>
-      <Logo />
-      <Heading as="h4">Login to your account</Heading>
-      <LoginForm />
+      <motion.div
+        initial={{ x: "-800px" }}
+        animate={{ x: "0px" }}
+        transition={{ duration: 0.3 }}
+      >
+        <Logo />
+      </motion.div>
+      <motion.div
+        initial={{ x: "800px" }}
+        animate={{ x: "0px" }}
+        transition={{ duration: 0.3 }}
+      >
+        <Heading as="h4">Login to your account</Heading>
+      </motion.div>{" "}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
+        <LoginForm />
+      </motion.div>
     </LoginLayout>
   );
 }
